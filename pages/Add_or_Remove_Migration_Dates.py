c15ca92b22e78d764cbec74415d1fe4d1b7101d4
Utsav Negi <55469943+negiutsav9@users.noreply.github.com>
2023-02-19 14:33:40 -0600

    Add files via upload

---
+++ pages/Add_or_Remove_Migration_Dates.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import os
+import numpy as np
+
+dates = pd.read_csv(os.getcwd() + '\pages\dates.csv', header=None) 
+
+st.title("Add or Remove Migration Dates")
+st.header("Current Migration Dates")
+
+st.sidebar.header("Add a Date")
+add_form = st.sidebar.form("add_form")
+new_date = add_form.date_input("New Migration Date")
+new_jira = add_form.text_input("Associated JIRA Issue")
+new_period = add_form.text_input("Pay Period ID")
+submitAdd = add_form.form_submit_button("Add the migration date")
+if(submitAdd):
+    newdata = [[new_date.strftime('%m/%d/%Y'),new_jira,new_period]]
+    new_df = pd.DataFrame(newdata, columns=["Migration Date","JIRA","Pay Period"])
+    new_df.to_csv(os.getcwd() + '/pages/dates.csv', mode='a', index=False, header=False)
+    st.experimental_rerun()
+
+st.sidebar.header("Remove a Date")
+remove_form = st.sidebar.form("remove_form")
+remove_date = remove_form.date_input("Migration Date to be removed")
+submitRemove = remove_form.form_submit_button("Remove the migration date")
+if(submitRemove):
+    remove_date = remove_date.strftime('%m/%d/%Y')
+    dates = dates.loc[dates[0] != remove_date]
+    dates.to_csv(os.getcwd() + '/pages/dates.csv', mode='w', index=False, header=False)
+    st.experimental_rerun()
+
+dates.columns = ["Migration Date","JIRA","Pay Period"]
+st.dataframe(dates, use_container_width=True)
